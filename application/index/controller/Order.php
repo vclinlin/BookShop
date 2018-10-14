b03f70e96b4dc47a03b2af8dd2b1749c63ce73cb
知否 <vclinlin@vip.qq.com>
2018-10-14 14:56:08 +0800

    购物车结算优化

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -9,14 +9,36 @@
 namespace app\index\controller;
 
 
+use app\index\model\Shopping_cart;
+use think\Session;
+
 class Order extends Shopping
 {
     protected $beforeActionList = [
         'checklogin'  =>  [
-            'except'=>'addorder'
+            'except'=>''
         ],
     ];
-    public function addOrder(){
+    public function addOrder($data){
         //获取购物车数据
+        $data = explode(',',$data);
+        $cart_model = new Shopping_cart();
+        $orderData = [];
+        foreach ($data as $item)
+        {
+            //查询该用户订单中库存足够的信息
+            if(!$rel = $cart_model->alias('A')->
+                join('books B','A.books_id = B.Id and B.count - B.sales >= A.sum')
+                ->where(['books_id'=>$item,'user_id'=>Session::get('user')])
+                ->field('A.sum,B.bookname,B.price,B.press')
+                ->select())
+            {
+                continue;
+            }
+            $orderData[] = $rel[0];
+        }
+        //满足条件的商品信息
+        $this->assign('validData',$orderData);
+        return $this->fetch();
     }
 }
